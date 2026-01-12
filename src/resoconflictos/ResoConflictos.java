7a8cba39a2a02bc3e7984fd65286a2c9ceeb5309
josito19 <jose.iglesias@iesnervion.es>
2026-01-12 12:26:25 +0100

    El estudiante 2 agrega código

--- src/resoconflictos/ResoConflictos.java
+++ src/resoconflictos/ResoConflictos.java
@@ -3,7 +3,7 @@ package resoconflictos;
 public class ResoConflictos {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hola soy el estudiante número dos y estoy realizando cambios.");
 		
 		
 	}
