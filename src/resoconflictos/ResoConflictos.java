8b7dd30fc152abc26b90bf05cde69d7f6c68f92e
josito19 <jose.iglesias@iesnervion.es>
2026-01-12 12:34:03 +0100

    comentario

--- src/resoconflictos/ResoConflictos.java
+++ src/resoconflictos/ResoConflictos.java
@@ -3,7 +3,7 @@ package resoconflictos;
 public class ResoConflictos {
 
 	public static void main(String[] args) {
-		
+		// Hay que comentar el código
 		System.out.println("Pero yo soy josé iglesias no estudiante 2");
 		
 	}
